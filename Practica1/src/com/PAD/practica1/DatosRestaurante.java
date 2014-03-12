9cf901751deab63b4117a32c51e1f0c5fdeddcc4
Volkjov <usuario_local@PTO0214>
2014-03-12 14:08:00 +0100

    a

--- Practica1/src/com/PAD/practica1/DatosRestaurante.java
+++ Practica1/src/com/PAD/practica1/DatosRestaurante.java
@@ -9,8 +9,26 @@ public class DatosRestaurante extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_datos_restaurante);
-	}
+        setContentView(R.layout.activity_datos_restaurante);
+        
+        Spinner sp = (Spinner) findViewById(R.id.spinner);
+        ArrayAdapter adapter = ArrayAdapter.createFromResource(
+                this, R.array.vias, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        sp.setAdapter(adapter);
+        
+        sp.setOnItemSelectedListener(new OnItemSelectedListener() {
+           
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, 
+            		int position, long id) {
+            	
+            	    
+            }
+                                 
+            public void onNothingSelected(AdapterView<?> parentView) {
+            	
+            }
+        });
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
